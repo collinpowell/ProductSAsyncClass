ed5c4e96539c8afe509715eb905b5dfc037917e8
collinpowell <collinskrubu723@gmail.com>
2024-04-12 12:55:39 +0100

    Updates

--- index.js
+++ index.js
@@ -1,27 +1,52 @@
-const productContainer = document.getElementById('product-cards')
+const productContainer = document.getElementById("product-cards");
 
-async function getProducts() {
-    console.log('Loading')
-  try {
-    const response = await axios({
-      method: "get",
-      url: "https://dummyjson.com/products",
+axios({
+  method: "get",
+  url: "https://dummyjson.com/products",
+})
+  .then((res) => {
+    console.log(res);
+    displayProducts(res.data.products);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+function displayProducts(products) {
+  products.map((product, i) => {
+    let starRating = "";
+
+    new Array(1, 2, 3, 4, 5).map((num, i) => {
+      console.log(Math.round(product.rating), num);
+      starRating +=
+        Math.round(product.rating) < num
+          ? `<svg
+      width="30"
+      height="28"
+      viewBox="0 0 30 28"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M15 0L18.3677 10.3647H29.2658L20.4491 16.7705L23.8168 27.1353L15 20.7295L6.18322 27.1353L9.55093 16.7705L0.734152 10.3647H11.6323L15 0Z"
+        fill="#D9D9D9"
+      />
+    </svg>`
+          : `<svg
+      width="30"
+      height="28"
+      viewBox="0 0 30 28"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M15 0L18.3677 10.3647H29.2658L20.4491 16.7705L23.8168 27.1353L15 20.7295L6.18322 27.1353L9.55093 16.7705L0.734152 10.3647H11.6323L15 0Z"
+        fill="#F5C64D"
+      />
+    </svg>`;
     });
 
-    console.log(response)
-    return response?.data?.products
-  } catch (error) {
-    alert(error.message);
-  }
-  console.log('Done')
-
-}
-
-async function displayProducts(){
-    const products = await getProducts()
-
-    products.map((product,i)=>{
-        productContainer.innerHTML+=`
+    productContainer.innerHTML += `
         <div id="product-card">
         <img src="${product.thumbnail}" alt="img" />
         <h3>${product.title}</h3>
@@ -32,79 +57,12 @@ async function displayProducts(){
         </div>
         <div class="footer">
           <div class="rating">
-            <svg
-              width="30"
-              height="28"
-              viewBox="0 0 30 28"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M15 0L18.3677 10.3647H29.2658L20.4491 16.7705L23.8168 27.1353L15 20.7295L6.18322 27.1353L9.55093 16.7705L0.734152 10.3647H11.6323L15 0Z"
-                fill="#F5C64D"
-              />
-            </svg>
-
-            <svg
-              width="30"
-              height="28"
-              viewBox="0 0 30 28"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M15 0L18.3677 10.3647H29.2658L20.4491 16.7705L23.8168 27.1353L15 20.7295L6.18322 27.1353L9.55093 16.7705L0.734152 10.3647H11.6323L15 0Z"
-                fill="#F5C64D"
-              />
-            </svg>
-
-            <svg
-              width="30"
-              height="28"
-              viewBox="0 0 30 28"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M15 0L18.3677 10.3647H29.2658L20.4491 16.7705L23.8168 27.1353L15 20.7295L6.18322 27.1353L9.55093 16.7705L0.734152 10.3647H11.6323L15 0Z"
-                fill="#F5C64D"
-              />
-            </svg>
-
-            <svg
-              width="30"
-              height="28"
-              viewBox="0 0 30 28"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M15 0L18.3677 10.3647H29.2658L20.4491 16.7705L23.8168 27.1353L15 20.7295L6.18322 27.1353L9.55093 16.7705L0.734152 10.3647H11.6323L15 0Z"
-                fill="#F5C64D"
-              />
-            </svg>
-
-            <svg
-              width="30"
-              height="28"
-              viewBox="0 0 30 28"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M15 0L18.3677 10.3647H29.2658L20.4491 16.7705L23.8168 27.1353L15 20.7295L6.18322 27.1353L9.55093 16.7705L0.734152 10.3647H11.6323L15 0Z"
-                fill="#D9D9D9"
-              />
-            </svg>
+            ${starRating}
           </div>
-          <p class="rating-text" style="margin: auto 0;">4.00</p>
+          <p class="rating-text" style="margin: auto 0;">${product.rating}</p>
         </div>
       </div>
         
-        `
-    })
+        `;
+  });
 }
-
-displayProducts()
-
-
